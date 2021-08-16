7a6d0460d48ffc30e8041095cce060ed0036ee5a
Maximilian6547s <maximilian.s517@gmail.com>
2021-08-16 13:34:47 +0300

    HeroService and Ktor

---
+++ hero/hero-datasource/src/main/java/com/maximcuker/hero_datasource/network/HeroServiceImpl.kt
@@ -0,0 +1,18 @@
+package com.maximcuker.hero_datasource.network
+
+import com.maximcuker.hero_datasource.network.model.HeroDto
+import com.maximcuker.hero_datasource.network.model.toHero
+import com.maximcuker.hero_domain.Hero
+import io.ktor.client.*
+import io.ktor.client.request.*
+
+class HeroServiceImpl(
+    private val httpClient: HttpClient
+) : HeroService {
+
+    override suspend fun getHeroStats(): List<Hero> {
+        return httpClient.get<List<HeroDto>> {
+            url(EndPoints.HERO_STATS)
+        }.map { it.toHero() }
+    }
+}
