831986ce0b229d6c498abbfa8472a9290bbffa6f
Maximilian6547s <maximilian.s517@gmail.com>
2021-08-16 13:22:41 +0300

    Hero Network Data Transfer Objects (DTO)

--- hero/hero-datasource/build.gradle.kts
+++ hero/hero-datasource/build.gradle.kts
@@ -3,5 +3,14 @@ apply {
     from("$rootDir/library-build.gradle")
 }
 
+plugins {
+    kotlin(KotlinPlugins.serialization) version Kotlin.version
+}
+
 dependencies {
+    "implementation"(project(Modules.heroDomain))
+
+    "implementation"(Ktor.core)
+    "implementation"(Ktor.clientSerialization)
+    "implementation"(Ktor.android)
 }
--- hero/hero-datasource/src/main/java/com/maximcuker/hero_datasource/Placeholder.kt
+++
@@ -1,4 +0,0 @@
-package com.maximcuker.hero_datasource
-
-class Placeholder {
-}
