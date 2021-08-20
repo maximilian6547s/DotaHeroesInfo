a6a832829b95e681509665071536d3bcab4f6392
Maximilian6547s <maximilian.s517@gmail.com>
2021-08-20 11:04:49 +0300

    Hero List Toolbar with Search Input

--- hero/ui-heroList/src/main/java/com/maximcuker/ui_herolist/ui/HeroList.kt
+++ hero/ui-heroList/src/main/java/com/maximcuker/ui_herolist/ui/HeroList.kt
@@ -1,18 +1,24 @@
 package com.codingwithmitch.ui_herolist
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import coil.ImageLoader
 import com.maximcuker.core.ProgressBarState
 import com.maximcuker.ui_herolist.HeroListItem
+import com.maximcuker.ui_herolist.components.HeroListToolbar
 import com.maximcuker.ui_herolist.ui.HeroListState
 
+@ExperimentalComposeUiApi
 @Composable
 fun HeroList(
     state: HeroListState,
@@ -22,19 +28,35 @@ fun HeroList(
     Box(
         modifier = Modifier.fillMaxSize()
     ) {
-        LazyColumn(
-            modifier = Modifier
-                .fillMaxSize()
-        ) {
-            items(state.heros) { hero ->
-                HeroListItem(
-                    hero = hero,
-                    onSelectHero = { heroId ->
-                        navigateToDetailScreen(heroId)
-                    },
-                    imageLoader = imageLoader,
-                )
+        Column {
+            val name = remember { mutableStateOf("")}
+            HeroListToolbar(
+                heroName = name.value,
+                onHeroNameChanged = {heroName ->
+                    name.value = heroName
+                },
+                onExecuteSearch = {
+                    //TODO
+                },
+                onShowFilterDialog = {
+                    //TODO
+                }
+            )
+            LazyColumn(
+                modifier = Modifier
+                    .fillMaxSize()
+            ) {
+                items(state.heros) { hero ->
+                    HeroListItem(
+                        hero = hero,
+                        onSelectHero = { heroId ->
+                            navigateToDetailScreen(heroId)
+                        },
+                        imageLoader = imageLoader,
+                    )
+                }
             }
+
         }
         if (state.progressBarState is ProgressBarState.Loading) {
             CircularProgressIndicator(
