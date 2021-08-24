3869063d73ce8e8630357c12ff48c76674ffff57
Maximilian6547s <maximilian.s517@gmail.com>
2021-08-24 13:49:11 +0300

    Get Heroes Test (Empty)

--- hero/hero-interactors/src/test/java/com/maximcuker/hero_interactors/GetHeroesTest.kt
+++ hero/hero-interactors/src/test/java/com/maximcuker/hero_interactors/GetHeroesTest.kt
@@ -109,4 +109,41 @@ class GetHeroesTest {
         // Confirm loading state is IDLE
         assert(emissions[3] == DataState.Loading<List<Hero>>(ProgressBarState.Idle))
     }
+
+    @Test
+    fun getHeroes_emptyList() =  runBlocking {
+        // setup
+        val heroDatabase = HeroDatabaseFake()
+        val heroCache = HeroCacheFake(heroDatabase)
+        val heroService = HeroServiceFake.build(
+            type = HeroServiceResponseType.EmptyList // Empty List
+        )
+
+        getHeroes = GetHeroes(
+            cache = heroCache,
+            service = heroService
+        )
+
+        // Confirm the cache is empty before any use-cases have been executed
+        var cachedHeroes = heroCache.selectAll()
+        assert(cachedHeroes.isEmpty())
+
+        // Execute the use-case
+        val emissions = getHeroes.execute().toList()
+
+        // First emission should be loading
+        assert(emissions[0] == DataState.Loading<List<Hero>>(ProgressBarState.Loading))
+
+        // Confirm second emission is data (empty list)
+        assert(emissions[1] is DataState.Data)
+        assert((emissions[1] as DataState.Data).data?.size?: 0 == 0)
+
+        // Confirm the cache is STILL EMPTY
+        cachedHeroes = heroCache.selectAll()
+        assert(cachedHeroes.isEmpty())
+
+        // Confirm loading state is IDLE
+        assert(emissions[2] == DataState.Loading<List<Hero>>(ProgressBarState.Idle))
+    }
+
 }
